daf022dea73294e11dfeb6ce1e5ce129931be1c1
kalic07 <kabir_alic@hotmail.com>
2026-06-08 21:47:10 +1000

    tt

--- FruitShop.Console/obj/Debug/net10.0/FruitShop.Console.AssemblyInfo.cs
+++ FruitShop.Console/obj/Debug/net10.0/FruitShop.Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FruitShop.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83f0a5e6dbcfd54f3a986469873e8fc9078328a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c1938af26cec82b36bf2d00c279b3af3ffc297f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FruitShop.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FruitShop.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
